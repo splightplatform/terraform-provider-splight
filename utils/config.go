4f4bd37ca868d297576ba6382989b4ab66d7f5c8
Matias Silva <matias.silva@splight-ae.com>
2024-03-18 15:34:30 -0300

    Allow the use of env vars for credentials

--- utils/config.go
+++ utils/config.go
@@ -36,7 +36,16 @@ func readConf(filename string) (*Config, error) {
 
 func TokenDefaultFunc() schema.SchemaDefaultFunc {
 	return func() (interface{}, error) {
+		access_id := os.Getenv("SPLIGHT_ACCESS_ID")
+		secret_key := os.Getenv("SPLIGHT_SECRET_KEY")
+		if access_id != "" && secret_key != "" {
+			return fmt.Sprintf("Splight %s %s", access_id, secret_key), nil
+		}
+
 		configFile := os.Getenv("HOME") + "/.splight/config"
+		if _, err := os.Stat(configFile); os.IsNotExist(err) {
+			return nil, fmt.Errorf("config file does not exist and no env vars found: %s", configFile)
+		}
 		config, err := readConf(configFile)
 		if err != nil {
 			return nil, err
@@ -48,7 +57,15 @@ func TokenDefaultFunc() schema.SchemaDefaultFunc {
 
 func HostnameDefaultFunc() schema.SchemaDefaultFunc {
 	return func() (interface{}, error) {
+		hostname := os.Getenv("SPLIGHT_PLATFORM_API_HOST")
+		if hostname != "" {
+			return hostname, nil
+		}
+
 		configFile := os.Getenv("HOME") + "/.splight/config"
+		if _, err := os.Stat(configFile); os.IsNotExist(err) {
+			return nil, fmt.Errorf("config file does not exist and no env vars found: %s", configFile)
+		}
 		config, err := readConf(configFile)
 		if err != nil {
 			return nil, err
