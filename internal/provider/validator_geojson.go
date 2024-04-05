d0f258d3da728d28997a0df8f6799eb846a968d4
smartinellimarco <smartinellimarco@gmail.com>
2024-04-05 19:23:47 -0300

    Change library since gojsonschema does not work properly

--- internal/provider/validator_geojson.go
+++ internal/provider/validator_geojson.go
@@ -2,50 +2,60 @@ package provider
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"runtime"
 
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
-	"github.com/xeipuuv/gojsonschema"
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	_ "github.com/santhosh-tekuri/jsonschema/v5/httploader"
 )
 
-type geoJSONValidator struct{}
+type geoJSONGeometryCollectionValidator struct{}
 
 // Ensure we implement the validator.String interface
-var _ validator.String = &geoJSONValidator{}
+var _ validator.String = &geoJSONGeometryCollectionValidator{}
 
-func (v geoJSONValidator) Description(ctx context.Context) string {
+func (v geoJSONGeometryCollectionValidator) Description(ctx context.Context) string {
 	return "GeoJSON RFC7946 validation"
 }
 
-func (v geoJSONValidator) MarkdownDescription(ctx context.Context) string {
+func (v geoJSONGeometryCollectionValidator) MarkdownDescription(ctx context.Context) string {
 	return "GeoJSON RFC7946 validation"
 }
 
 // Validate runs the main validation logic of the validator, reading configuration data out of `req` and updating `resp` with diagnostics.
-func (v geoJSONValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+func (v geoJSONGeometryCollectionValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
 
 	// If the value is unknown or null, there is nothing to validate.
 	if req.ConfigValue.IsUnknown() || req.ConfigValue.IsNull() {
 		return
 	}
 
-	runtime.Breakpoint()
-	data := req.ConfigValue.ValueString()
-	schemaLoader := gojsonschema.NewReferenceLoader("https://geojson.org/schema/GeoJSON.json")
-	documentLoader := gojsonschema.NewReferenceLoader(data)
+	var raw_json interface{}
+	if err := json.Unmarshal([]byte(req.ConfigValue.ValueString()), &raw_json); err != nil {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Error decoding Geometry Collection. Is it a valid JSON?",
+			fmt.Sprintf("Error: %s", err),
+		)
+	}
 
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	schema, err := jsonschema.Compile("https://geojson.org/schema/GeoJSON.json")
+
 	if err != nil {
-		panic(err.Error())
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Error while retrieving JSON Schema",
+			fmt.Sprintf("Error: %s", err),
+		)
 	}
 
-	if result.Valid() {
-		fmt.Printf("The document is valid\n")
-	} else {
-		fmt.Printf("The document is not valid. see errors :\n")
-		for _, desc := range result.Errors() {
-			fmt.Printf("- %s\n", desc)
-		}
+	if err = schema.Validate(raw_json); err != nil {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Error while validating Geometry Collection",
+			"Make sure it satifies RFC7946: https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.8",
+		)
 	}
+
 }
