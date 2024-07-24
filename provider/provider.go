50aa000f961dfd03b5cf2a88ff0797627557006f
franco <francozacco@gmail.com>
2024-07-24 12:08:41 +0200

    histogram chart

--- provider/provider.go
+++ provider/provider.go
@@ -31,6 +31,7 @@ func Provider() *schema.Provider {
 			"splight_dashboard_tab":              resourceDashboardTab(),
 			"splight_dashboard_table_chart":      resourceDashboardTableChart(),
 			"splight_dashboard_timeseries_chart": resourceDashboardTimeseriesChart(),
+			"splight_dashboard_histogram_chart":  resourceDashboardHistogramChart(),
 			"splight_file":                       resourceFile(),
 			"splight_file_folder":                resourceFileFolder(),
 			"splight_function":                   resourceFunction(),
