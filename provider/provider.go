0e6d8b0fb464dc01a154ff3298c7f01ee3f9a318
franco <francozacco@gmail.com>
2024-07-25 15:44:00 +0200

    aaand finally, action list chart

--- provider/provider.go
+++ provider/provider.go
@@ -42,6 +42,7 @@ func Provider() *schema.Provider {
 			"splight_dashboard_gauge_chart":       resourceDashboardGaugeChart(),
 			"splight_dashboard_alertlist_chart":   resourceDashboardAlertListChart(),
 			"splight_dashboard_assetlist_chart":   resourceDashboardAssetListChart(),
+			"splight_dashboard_actionlist_chart":  resourceDashboardActionListChart(),
 			"splight_file":                        resourceFile(),
 			"splight_file_folder":                 resourceFileFolder(),
 			"splight_function":                    resourceFunction(),
