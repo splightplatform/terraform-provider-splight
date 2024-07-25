b4cea9d1cf8a41250ea09a70ff01f0b98d20edb4
franco <francozacco@gmail.com>
2024-07-25 15:02:33 +0200

    alert list chart

---
+++ api/client/dashboard_alertlist_chart.go
@@ -0,0 +1,91 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+type DashboardAlertListChartParams struct {
+	DashboardChartParams
+	Type          string   `json:"type"`
+	FilterName    string   `json:"filter_name,omitempty"`
+	FilterStatus  []string `json:"filter_status"`
+	AlertListType string   `json:"alert_list_type,omitempty"`
+}
+
+type DashboardAlertListChart struct {
+	DashboardAlertListChartParams
+	ID string `json:"id"`
+}
+
+func (c *Client) ListDashboardAlertListCharts() (*map[string]DashboardAlertListChart, error) {
+	body, err := c.httpRequest("v2/engine/dashboard/charts/", "GET", bytes.Buffer{})
+	if err != nil {
+		return nil, err
+	}
+	items := map[string]DashboardAlertListChart{}
+	err = json.NewDecoder(body).Decode(&items)
+	if err != nil {
+		return nil, err
+	}
+	return &items, nil
+}
+
+func (c *Client) CreateDashboardAlertListChart(item *DashboardAlertListChartParams) (*DashboardAlertListChart, error) {
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(item)
+	if err != nil {
+		return nil, err
+	}
+	body, err := c.httpRequest("v2/engine/dashboard/charts/", "POST", buf)
+	if err != nil {
+		return nil, err
+	}
+
+	asset := &DashboardAlertListChart{}
+	err = json.NewDecoder(body).Decode(asset)
+	if err != nil {
+		return nil, err
+	}
+	return asset, nil
+}
+
+func (c *Client) UpdateDashboardAlertListChart(id string, item *DashboardAlertListChartParams) (*DashboardAlertListChart, error) {
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(item)
+	if err != nil {
+		return nil, err
+	}
+	body, err := c.httpRequest(fmt.Sprintf("v2/engine/dashboard/charts/%s/", id), "PUT", buf)
+	if err != nil {
+		return nil, err
+	}
+	asset := &DashboardAlertListChart{}
+	err = json.NewDecoder(body).Decode(asset)
+	if err != nil {
+		return nil, err
+	}
+	return asset, nil
+}
+
+func (c *Client) RetrieveDashboardAlertListChart(id string) (*DashboardAlertListChart, error) {
+	body, err := c.httpRequest(fmt.Sprintf("v2/engine/dashboard/charts/%s/", id), "GET", bytes.Buffer{})
+	if err != nil {
+		return nil, err
+	}
+	asset := &DashboardAlertListChart{}
+	err = json.NewDecoder(body).Decode(asset)
+	if err != nil {
+		return nil, err
+	}
+	return asset, nil
+}
+
+func (c *Client) DeleteDashboardAlertListChart(id string) error {
+	_, err := c.httpRequest(fmt.Sprintf("v2/engine/dashboard/charts/%s/", id), "DELETE", bytes.Buffer{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
