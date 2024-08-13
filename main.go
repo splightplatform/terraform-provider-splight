f70b1a2f341ca1d684d1fe08abdfb746c83737ca
smartinellimarco <smartinellimarco@gmail.com>
2024-08-13 15:57:06 -0300

    Refactor main

--- main.go
+++ main.go
@@ -14,34 +14,31 @@ import (
 	"github.com/splightplatform/terraform-provider-splight/provider"
 )
 
-// Version is initialized by the Go linker to contain the semver of this build.
-var Version string = "dev"
-
 func main() {
-	ctx := context.Background()
-
-	// Define and parse command-line flags.
 	var debug bool
+
+	// Parse command-line flags.
 	flag.BoolVar(&debug, "debug", false, "Enable debugging support for tools like delve")
 	flag.Parse()
 
 	// Upgrade the provider to the TF6 protocol version.
 	upgradedSdkProvider, err := tf5to6server.UpgradeServer(
-		ctx,
+		context.Background(),
 		provider.Provider().GRPCProvider,
 	)
+
 	if err != nil {
 		log.Fatalf("Failed to upgrade provider server: %v", err)
 	}
 
 	// Serve the provider with or without debugging based on the flag.
-	if err := serveProvider(ctx, upgradedSdkProvider, debug); err != nil {
+	if err := serveProvider(upgradedSdkProvider, debug); err != nil {
 		log.Fatalf("Failed to serve provider: %v", err)
 	}
 }
 
 // serveProvider sets up the provider server with optional debugging.
-func serveProvider(ctx context.Context, providerServer tfprotov6.ProviderServer, debug bool) error {
+func serveProvider(providerServer tfprotov6.ProviderServer, debug bool) error {
 	if debug {
 		return tf6server.Serve(
 			"registry.terraform.io/splightplatform/splight",
