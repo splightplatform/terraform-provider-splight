9933652e231fbd6e069a2c8c465ac15748a8c1ae
smartinellimarco <smartinellimarco@icloud.com>
2025-02-05 12:46:29 -0300

    Fix url

--- splight/client/models/slack_generator.go
+++ splight/client/models/slack_generator.go
@@ -25,7 +25,7 @@ func (m *SlackGenerator) GetParams() Params {
 }
 
 func (m *SlackGenerator) ResourcePath() string {
-	return "v2/engine/asset/slack_generators/"
+	return "v2/engine/asset/slack-generators/"
 }
 
 func (m *SlackGenerator) FromSchema(d *schema.ResourceData) error {
